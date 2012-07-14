147f44cc3fa32e26eb1c1d08bdcd32cd7a969f85
Victor Pineda <victor.pineda.ramos@gmail.com>
2012-07-14 13:52:41 -0500

    new file:   .gitignore 	new file:   Gemfile 	new file:   Gemfile.lock 	new file:   README.rdoc 	new file:   Rakefile 	new file:   app/assets/images/rails.png 	new file:   app/assets/javascripts/application.js 	new file:   app/assets/javascripts/products.js.coffee 	new file:   app/assets/javascripts/users.js.coffee 	new file:   app/assets/stylesheets/application.css 	new file:   app/assets/stylesheets/products.css.scss 	new file:   app/assets/stylesheets/scaffolds.css.scss 	new file:   app/assets/stylesheets/users.css.scss 	new file:   app/controllers/application_controller.rb 	new file:   app/controllers/products_controller.rb 	new file:   app/controllers/users_controller.rb 	new file:   app/helpers/application_helper.rb 	new file:   app/helpers/products_helper.rb 	new file:   app/helpers/users_helper.rb 	new file:   app/mailers/.gitkeep 	new file:   app/models/.gitkeep 	new file:   app/models/product.rb 	new file:   app/models/user.rb 	new file:   app/views/layouts/application.html.erb 	new file:   app/views/products/_form.html.erb 	new file:   app/views/products/edit.html.erb 	new file:   app/views/products/index.html.erb 	new file:   app/views/products/new.html.erb 	new file:   app/views/products/show.html.erb 	new file:   app/views/users/_form.html.erb 	new file:   app/views/users/add_product.html.erb 	new file:   app/views/users/edit.html.erb 	new file:   app/views/users/index.html.erb 	new file:   app/views/users/new.html.erb 	new file:   app/views/users/show.html.erb 	new file:   config.ru 	new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/initializers/wrap_parameters.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   db/migrate/20120714172552_create_users.rb 	new file:   db/migrate/20120714172853_create_products.rb 	new file:   db/migrate/20120714174010_add.rb 	new file:   db/migrate/20120714181806_add_product_to_user.rb 	new file:   db/schema.rb 	new file:   db/seeds.rb 	new file:   doc/README_FOR_APP 	new file:   lib/assets/.gitkeep 	new file:   lib/tasks/.gitkeep 	new file:   log/.gitkeep 	new file:   public/404.html 	new file:   public/422.html 	new file:   public/500.html 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/robots.txt 	new file:   script/rails 	new file:   test/fixtures/.gitkeep 	new file:   test/fixtures/products.yml 	new file:   test/fixtures/users.yml 	new file:   test/functional/.gitkeep 	new file:   test/functional/products_controller_test.rb 	new file:   test/functional/users_controller_test.rb 	new file:   test/integration/.gitkeep 	new file:   test/performance/browsing_test.rb 	new file:   test/test_helper.rb 	new file:   test/unit/.gitkeep 	new file:   test/unit/helpers/products_helper_test.rb 	new file:   test/unit/helpers/users_helper_test.rb 	new file:   test/unit/product_test.rb 	new file:   test/unit/user_test.rb 	new file:   vendor/assets/javascripts/.gitkeep 	new file:   vendor/assets/stylesheets/.gitkeep 	new file:   vendor/plugins/.gitkeep

---
+++ Tienda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tienda::Application.config.secret_token = '89dc8afba011458f74795577b430c05e8db10cb9d577d05bcbd9baccb5ef2cc25d6e16d06c1f4e43d0c8e93581a8c8b6c7b9f9e34b1ac7a91d12f816587880fd'
---
+++ Tienda/db/migrate/20120714181806_add_product_to_user.rb
@@ -0,0 +1,4 @@
+class AddProductToUser < ActiveRecord::Migration
+  def change
+  end
+end
